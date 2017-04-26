d481e43b75bab96f5d8f17f0162f3778bfd6a41d
Sean Kim <seaneasy@D-108-179-152-101.dhcp4.washington.edu>
2017-04-26 14:59:51 -0700

    sdf

--- exercise-4/exercise.r
+++ exercise-4/exercise.r
@@ -4,19 +4,19 @@ library(httr)
 
 # Make a variable base.url that has the same base url from the omdb documentation.
 # (Hint: visit https://www.omdbapi.com/ to find the base url)
-
+base.url <- "http://www.omdbapi.com/?"
 
 # Make a variable called movie that has the name of your favorite movie
-
+movie <- "Get Out"
 
 # Make a variable called "query.parameters" that holds a list of the parameters
 # to pass to the API. View the OMDb documentation to see which parameters
 # are available.
-
+query.parameters <- list(t = movie, r = "json")
 
 # Make a variable called request that is a string of a request URL made up of the base URL
 # and the parameters string
-
+request <- 
 
 # Use fromJSON to retrieve JSON data from the omdb api using your request.
 # Store the result in a variable called movie.data
@@ -42,8 +42,8 @@ library(httr)
 
 # Write a function to make your API request and process the data
 
-  # Make a variable called request that is a string of a request URL made up of the base URL
-  # and the parameters string
+# Make a variable called request that is a string of a request URL made up of the base URL
+# and the parameters string
 
 # For every entry in the vector api.request, APPLY the function fromJSON to make a list of lists
 # one entry for each request and assign this to a variable called data. 
